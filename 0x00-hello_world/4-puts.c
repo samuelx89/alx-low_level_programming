85a28940e5bc65f07ddde759a0cc96973ff86757
samuelx89 <samuelx89@hotmail.com>
2022-09-09 03:22:16 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
@@ -13,4 +13,4 @@ int main(void)
 }
 
 
-/* ========================= */
+/* ========================= *i/
