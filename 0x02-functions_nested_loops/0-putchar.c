e9798e78f12bc9613871c5e318e8ffda7aacf13a
samuelx89 <samuelx89@hotmail.com>
2022-09-14 02:03:45 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+#include "main.h"
+
 /**
- *  * main - prints _putchar
- *   *
- *    * return: always 0 (Success)
- */
+* main -  prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
-
