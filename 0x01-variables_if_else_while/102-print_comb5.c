6964603b2c673054dc6daeaec6eccce3c5cced73
samuelx89 <samuelx89@hotmail.com>
2022-09-14 12:41:53 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,51 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * * main - Entry point
- * : Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
-	while (p < 58)
-	{
-		while (q < 58)
-		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 && q == 56 && y == 57 && x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
-		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
-	}
-	putchar('\n');
-	return (0);
+int i = 48, j = 48, k = 48, l = 49;
+while  (i < 58)
+{
+putchar(i);
+putchar(j);
+putchar(32);
+putchar(k);
+putchar(l);
+if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+{
+putchar('\n');
+i++;
 }
+else
+{
+putchar(44);
+putchar(32);
+if ((k == 57) && (l == 57))
+{
+if (j < 56)
+l = ++j + 1, k = i;
+else if (j == 56)
+{
+j++;
+k = i + 1, l = 48;
+}
+else if (j == 57)
+	j = 48, l = 49, k = ++i;
+	}
+else if (l < 57)
+{
+l++;
+}
+else
+{
+l = 48, k++;
+}
+}
+}
+return (0);
+}}
