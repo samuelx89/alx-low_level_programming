592cc6d17018ff10a3afc4ee2afe5d73aa127329
samuelx89 <samuelx89@hotmail.com>
2022-09-19 12:13:06 -0700

    strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-nt _putchar(char c);
+#ifndef FILE_MAIN
+#define FILE_MAIN
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#endif
