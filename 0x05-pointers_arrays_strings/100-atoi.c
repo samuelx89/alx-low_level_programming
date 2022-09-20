aa47ed73c00f3de4981802022561bca0a8cb239d
samuelx89 <samuelx89@hotmail.com>
2022-09-19 23:31:43 -0700

    lol

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,9 +13,9 @@ do {
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
-num = (num * 10) + (*s - '0');
+num = (num *10) + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
-return (num * sign);
+return (num *sign);
 }
